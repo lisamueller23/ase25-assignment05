30b7242b0970a452630b4e77fcf1744fb9766e7b
lisamueller23 <94631907+lisamueller23@users.noreply.github.com>
2026-01-16 20:31:09 +0100

    Update UserController.java

--- api/src/main/java/de/unibayreuth/se/taskboard/api/controller/UserController.java
+++ api/src/main/java/de/unibayreuth/se/taskboard/api/controller/UserController.java
@@ -6,6 +6,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import de.unibayreuth.se.taskboard.api.dtos.UserDto;
+import de.unibayreuth.se.taskboard.business.ports.UserService;
+
+import java.util.List;
+import java.util.UUID;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -18,7 +23,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     // TODO: Add GET /api/users endpoint to retrieve all users.
+    @GetMapping
+    @ResponseBody
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
     // TODO: Add GET /api/users/{id} endpoint to retrieve a user by ID.
+    @GetMapping("/{id}")
+    @ResponseBody
+    public UserDto getUserById(@PathVariable("id") UUID id) {
+        return userService.getUserById(id);
+    }
+
     // TODO: Add POST /api/users endpoint to create a new user based on a provided user DTO.
+    @PostMapping
+    @ResponseBody
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        return userService.createUser(userDto);
+    }
 }
