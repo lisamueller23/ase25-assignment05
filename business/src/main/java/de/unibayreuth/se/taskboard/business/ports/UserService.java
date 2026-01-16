3d9515a9207a63924e7b730cf3befb5983ea3cf5
lisamueller23 <94631907+lisamueller23@users.noreply.github.com>
2026-01-16 20:33:07 +0100

    Update UserService.java

--- business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
@@ -1,6 +1,15 @@
 package de.unibayreuth.se.taskboard.business.ports;
 
+import de.unibayreuth.se.taskboard.business.domain.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
     //TODO: Add user service interface that the controller uses to interact with the business layer.
+    List<User> getAllUsers();
+    User getUserById(UUID id);
+    User createUser(User user);
+
     //TODO: Implement the user service interface in the business layer, using the existing user persistence service.
 }
