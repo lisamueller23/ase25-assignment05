2ee92f6f313774b43b767467017cd12117999b8b
lisamueller23 <94631907+lisamueller23@users.noreply.github.com>
2026-01-16 20:31:48 +0100

    Update UserDto.java

--- api/src/main/java/de/unibayreuth/se/taskboard/api/dtos/UserDto.java
+++ api/src/main/java/de/unibayreuth/se/taskboard/api/dtos/UserDto.java
@@ -1,5 +1,8 @@
 package de.unibayreuth.se.taskboard.api.dtos;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 //TODO: Add DTO for users.
-public record UserDto(
+public record UserDto(UUID id, LocalDateTime createdAt, String name
 ) { }
